7e3768074cfefb0bc81945787797f58f6859148d
XaGGeR <haggeroy@gmail.com>
2016-02-25 04:25:13 +0300

    That's all I need to commit

--- binary-tree.js
+++ binary-tree.js
@@ -1,76 +1,112 @@
 'use strict';
-
-function BinaryTree() {
-	this.root = null;
-	this._size = 0;
-	
-	// public methods
-	this.insert = function (value) {
-		var node = {
-				data: value,
-				left: null,
-				right: null,
-		};
-		
-		if (this.root === null) {
-			this.root = node;
-		} else {
-			insertR(this.root, node);
-		}
-		
-		this._size += 1;
-	}	
-
-	this.remove = function () {
-		if (this.root === null) {
-			return;
-		}
-				
-		this._size -= 1;
-	}
-
-	this.contains = function (value) {
-		if (this.root === null) {
-			return false;
-		} else {
-			return containsR(this.root, value);
-		}
-	}
-	
-	this.size = function () {
-		return this._size;
-	}
-
-	this.isEmpty = function() {
-		return (this.root === null);
-	}
-
-	// private methods
-	var insertR = function (curNode, newNode) {
-		if (curNode.data > newNode.data) {
-			if (curNode.left === null) {
-				curNode.left = newNode;
-			} else {
-				insertR(curNode.left, newNode);
-			}
-		} else {
-			if (curNode.right === null) {
-				curNode.right = newNode;
-			} else {
-				insertR(curNode.right, newNode);
-			}
-		}
-	}
-	
-	var containsR = function (node, value) {
-		if (node === null) {
-			return false;
-		} else if (node.data === value) {
-			return true;
-		} else if (node.data > value) {
-			return containsR(node.left, value);
-		} else {
-			return containsR(node.right, value);	
-		}
-	}
+ 
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+ 
+    insert(data) {
+        var node = new Node(data, null, null);
+        if (this.root == null) {
+            this.root = node;
+        } else {
+            return insertR(this.root, data);
+        }
+    }
+ 
+    contains(data) {
+        if (this.root == null) {
+            return false;
+        } else {
+            return containsR(this.root, data);
+        }
+    }
+ 
+    remove(data) {
+        if (this.contains(data) == true) {
+            this.root = removeR(this.root, data);
+        }
+    }
+ 
+    size() {
+        function iter(node) {
+            if (!node) {
+                return 0;
+            } else {
+                return 1 + iter(node.left) + iter(node.right);
+            }
+        }
+ 
+        return iter(this.root);
+    }
+ 
+ 
+    isEmpty() {
+        return (this.root === null);
+    }
 }
+ 
+// private methods
+var insertR = function (node, value) {
+    if (node.data > value) {
+        if (node.left == null) {
+            node.left = new Node(value);
+        } else {
+            insertR(node.left, value);
+        }
+    } else {
+        if (node.right == null) {
+            node.right = new Node(value);
+        } else {
+            insertR(node.right, value);
+        }
+    }
+};
+ 
+var containsR = function (node, value) {
+    if (node == null) {
+        return false;
+    } else if (node.data == value) {
+        return true;
+    } else if (node.data > value) {
+        return containsR(node.left, value);
+    } else {
+        return containsR(node.right, value);
+    }
+};
+ 
+var removeR = function (node, value) {
+    function nodeValues(node) {
+        if (node) {
+            return [node.data].concat(nodeValues(node.right)).concat(nodeValues(node.left));
+        } else {
+            return [];
+        }
+    }
+ 
+    function extend(node1, node2) {
+        nodeValues(node2).forEach(function (val) {
+            insertR(node1, val);
+        });
+ 
+        return node1;
+    }
+ 
+    if (node.data === value) {
+        if (node.right || node.left) {
+            if (node.right) {
+                return extend(node.right, node.left);
+            } else {
+                return extend(node.left, node.right);
+            }
+        } else {
+            return null;
+        }
+    } else if (value > node.data) {
+        node.right = removeR(node.right, value);
+    } else {
+        node.left = removeR(node.left, value);
+    }
+ 
+    return node;
+};
